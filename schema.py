4f8cc0c307bb95288907522fc14d8ef40e97534b
nzioker <nzioker@gmail.com>
2025-12-14 12:28:34 +0300

    Delete schema.py

--- schema.py
+++
@@ -1,9 +0,0 @@
-import graphene
-
-class Query(graphene.ObjectType):
-    hello = graphene.String()
-    
-    def resolve_hello(self, info):
-        return "Hello, GraphQL!"
-
-schema = graphene.Schema(query=Query)
