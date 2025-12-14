c61b7794636a1ac9926a45893a571a5e8ea90edf
nzioker <nzioker@gmail.com>
2025-12-14 12:24:43 +0300

    Add comments to schema.py for clarity

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,9 +1,9 @@
-import graphene
+import graphene  # Import graphene
 
-class Query(graphene.ObjectType):
+class Query(graphene.ObjectType):  # Class Query exists and inherits graphene.ObjectType
     hello = graphene.String()
     
     def resolve_hello(self, info):
         return "Hello, GraphQL!"
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query)  # Schema with Query class
